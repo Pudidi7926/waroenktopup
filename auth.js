2cea257b65a37b8394335dbe210440acd9a6683a
Pudidi7926 <100346540+Pudidi7926@users.noreply.github.com>
2025-05-24 23:30:58 +0700

    wdw

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = 'https://pmbpaxvczqgrqzclikon.supabase.co'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtYnBheHZjenFncnF6Y2xpa29uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxMDEwMDksImV4cCI6MjA2MzY3NzAwOX0.UV3lRlr9O8R7sHUnAiGmmf24fT-S0XlTre3xqFyoIXc'
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+const supabase = supabase.createClient(supabaseUrl, supabaseKey)
 
 async function handleRegister(e) {
   e.preventDefault()
